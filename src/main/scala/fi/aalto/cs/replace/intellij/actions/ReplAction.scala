1a1a447e7d33bac8e021db773c9c37ff177a217e
mai1 <ilona.ma@aalto.fi>
2023-08-23 19:30:00 +0300

    Comment out dialog (temporary "fix")

--- src/main/scala/fi/aalto/cs/replace/intellij/actions/ReplAction.scala
+++ src/main/scala/fi/aalto/cs/replace/intellij/actions/ReplAction.scala
@@ -175,10 +175,10 @@ class ReplAction extends RunConsoleAction:
   private def showReplDialog(@NotNull project: Project,
                              @NotNull module: Module): ReplConfigurationFormModel =
     val configModel = new ReplConfigurationFormModel(project, ModuleUtils.getModuleDirectory(module), module.getName)
-    val configForm = new ReplConfigurationForm(configModel, project)
-    val configDialog = new ReplConfigurationDialog
-    configDialog.setReplConfigurationForm(configForm)
-    configDialog.setVisible(true)
+//    val configForm = new ReplConfigurationForm(configModel, project)
+//    val configDialog = new ReplConfigurationDialog
+//    configDialog.setReplConfigurationForm(configForm)
+//    configDialog.setVisible(true)
     configModel
 
   private def getScalaModuleOfEditorFile(@NotNull project: Project,
