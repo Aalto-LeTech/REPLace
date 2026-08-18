111848ae4d960ec05d99c503de6b6fc21eaa351f
Jaakko Nakaza <jaakko@nakaza.me>
2026-08-18 23:47:08 +0900

    chore: upgrade IntelliJ platform to 2026.2 and Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+val scalaCompileVersion = "3.7.1"
+val scalaReplTestRuntime = configurations.create("scalaReplTestRuntime")
+
 plugins {
-    id("org.jetbrains.intellij.platform") version "2.6.0"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
     id("scala")
 }
 
@@ -16,18 +21,32 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        intellijIdeaCommunity("2025.2")
+        intellijIdea("2026.2.0.1")
         bundledPlugin("com.intellij.java")
-        bundledPlugin("com.intellij.properties")
         compatiblePlugin("org.intellij.scala")
+        testFramework(TestFrameworkType.Platform)
+
+        plugin("fi.aalto.cs.intellij-plugin:4.4.2")
+        plugin("fi.aalto.cs.inspections:1.1.0")
     }
 
-    compileOnly("org.scala-lang:scala3-library_3:3.7.1")
+    compileOnly("org.scala-lang:scala3-library_3:$scalaCompileVersion")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.scala-lang:scala3-compiler_3:$scalaCompileVersion")
+    scalaReplTestRuntime("org.scala-lang:scala3-repl_3:3.8.2")
+    scalaReplTestRuntime("org.scala-lang:scala-library:3.8.2")
 }
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "252"
+            sinceBuild = "262"
         }
     }
 }
